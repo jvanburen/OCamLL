42f515e40aaa4098c28c27cdb425d4a6e18cc628
Jacob Van Buren <jvanbure@andrew.cmu.edu>
2017-05-01 13:44:18 -0400

    refactored into modules

--- middle_end/array_optimizations.ml
+++ middle_end/array_optimizations.ml
@@ -18,130 +18,260 @@
 let pass_name = "optimize-array-accesses"
 let () = Clflags.all_passes := pass_name :: !Clflags.all_passes
 
-module VarMap = Variable.Map (*  Map.Make (Variable) *)
-type atom = | IntAtom of int | LenAtom of Variable.t | VarAtom of Variable.t
-type upperBound = | UBUndef | UB of atom option
-type lowerBound = | LBUndef | LB of int option
-(* Ranges are half-open *)
-type scalarConstraint = lowerBound * upperBound
-type boolConstraints = {
-    iftrue: scalarConstraint VarMap.t;
-    iffalse: scalarConstraint VarMap.t;
-  }
-type varInfo =
-  | BoolInfo of boolConstraints
-  | ScalarInfo of scalarConstraint
-  | Other
-type lattice = varInfo VarMap.t
-let getVarInfo (var : Variable.t) (map : lattice) =
-  if VarMap.mem var map
-  then VarMap.find var map
-  else Other
-                
-let merge_atom atomA atomB = match (atomA, atomB) with
-  | (IntAtom i, IntAtom j) -> Some (IntAtom (min i j))
-  | (LenAtom v1, LenAtom v2) -> if v1 = v2 then Some (LenAtom v1) else None
-  | (VarAtom v1, VarAtom v2) -> if v1 = v2 then Some (VarAtom v1) else None
-  | _ -> None
+exception Impossible
+exception TypeMismatch
 
-(* TODO: Check for correctness *)
-let merge_upper_bound ub1 ub2 =
-  match (ub1, ub2) with
-  | (UBUndef, _) -> ub2
-  | (_, UBUndef) -> ub1
-  | (UB (Some atom1), UB (Some atom2)) -> UB (merge_atom atom1 atom2)
-  | _ -> UB None
+(* module type BOUNDED_SEMILATTICE =
+sig
+  type t
+  val bot : t
+  val top : t option
+  val join : t -> t -> t
+  val merge : t -> t -> t
+  val leq : t -> t -> bool
+  exception WeirdOp of t * t
+end *)
 
-let merge_lower_bound lb1 lb2 =
-  match (lb1, lb2) with
-  | (_, LBUndef) -> lb1 
-  | (LBUndef, _) -> lb2
-  | (LB (Some x), LB (Some y)) -> LB (Some (min x y))
-  | _ -> LB None
-(* TODO: Actually merge the two constraints*)
-let merge_bool_constraints (_ : boolConstraints) (_ : boolConstraints) =
-  {iffalse = VarMap.empty; iftrue = VarMap.empty}
-let merge_scalars (lb1, ub1) (lb2, ub2) = (merge_lower_bound lb1 lb2,
-                                           merge_upper_bound ub1 ub2)
-let merge_var_info vi1 vi2 =
-  match (vi1, vi2) with
-  | (BoolInfo bc1, BoolInfo bc2) -> BoolInfo (merge_bool_constraints bc1 bc2)
-  | (ScalarInfo si1, ScalarInfo si2) -> ScalarInfo (merge_scalars si1 si2)
-  | (Other, y) -> y
-  | (x, Other) -> x
-  (* TODO: This should be impossible. Raise an exception? *)
-  | _ -> Other
+(* TODO: implement widening operator *)
+module Widening =
+struct
+  let min x y = Pervasives.min x y
+  let max x y = Pervasives.max x y
+end
 
-let rec add_constraints (known : lattice) (flam : Flambda.t) : (lattice * varInfo) =
+module UpperBound =
+struct
+  type atom =
+    | IntAtom of int
+    | LenAtom of Variable.t
+    | VarAtom of Variable.t
+  type t =
+    | PosInf
+    | UB of atom
+    | Undef
+  let (bot, top) = (Undef, PosInf)
+
+  (* Don't use this externally... not sure how to hide, there's no `local` kw *)
+  let atomOp oper s k a b =
+    match (a, b) with
+    | (IntAtom x, IntAtom y) -> s (IntAtom (oper x y)) (* TODO: add widening operator *)
+    | (LenAtom x, LenAtom y) -> if x = y then s a else k ()
+    | (VarAtom x, VarAtom y) -> if x = y then s a else k ()
+    | _ -> k ()
+
+  let join a b =
+    match (a, b) with
+    | (Undef, other) -> other
+    | (other, Undef) -> other
+    | (UB aa, UB bb) -> atomOp Widening.max (fun x -> UB x) (fun () -> top) aa bb
+    | _ -> top
+  let meet a b =
+    match (a, b) with
+    | (PosInf, other) -> other
+    | (other, PosInf) -> other
+    | (UB aa, UB bb) -> atomOp Widening.min (fun x -> UB x) (fun () -> bot) aa bb
+    | _ -> bot
+  let leq a b = (join a b) = b
+end
+module UB = UpperBound (* Abbreviation *)
+
+module LowerBound =
+struct
+  type t =
+    | NegInf
+    | LB of int
+    | Undef
+  let (bot, top) = (Undef, NegInf)
+
+  let join a b =
+    match (a, b) with
+    | (Undef, other) -> other
+    | (other, Undef) -> other
+    | (LB aa, LB bb) -> LB (Widening.min aa bb)
+    | _ -> top
+  let meet a b =
+    match (a, b) with
+    | (NegInf, other) -> other
+    | (other, NegInf) -> other
+    | (LB aa, LB bb) -> LB (Widening.max aa bb)
+    | _ -> bot
+  let leq a b = (join a b) = b
+end
+module LB = LowerBound (* Abbreviation *)
+
+
+module ScalarConstraint =
+struct
+  type t = LB.t * UB.t (* Represents a half-open range *)
+  let bot = (LB.bot, UB.bot)
+  let top = (LB.NegInf, UB.PosInf)
+  let join (alb, aub) (blb, bub) = (LB.join alb blb, UB.join aub bub)
+  let meet (alb, aub) (blb, bub) = (LB.meet alb blb, UB.meet aub bub)
+  let leq a b = (join a b) = b
+end
+module SC = ScalarConstraint
+
+(* TODO: Fix uses of add to join? *)
+module Lattice =
+struct
+  module VarMap =
+  struct
+    include Variable.Map
+    let addOption k v map =
+      match v with
+      | Some v -> add k v map
+      | None -> map
+  end
+
+  type t = varInfo VarMap.t
+  and varInfo =
+    | BoolInfo of boolConstraints
+    | ScalarInfo of ScalarConstraint.t
+    | NoInfo
+  and boolConstraints = { ifTrue: t; ifFalse: t; }
+  (* boolConstraints is a Map from variable value to constraints *)
+
+  let bot = VarMap.empty
+  (* Top doesn't exist, we don't know what the universe of keys is *)
+
+  let rec join a b =
+    let f _ a b =
+      match (a, b) with
+      | (Some aa, Some bb) -> Some (joinVarInfo aa bb)
+      | (Some _, None) -> a
+      | (None, Some _) -> b
+      | (None, None) -> raise Impossible
+    in VarMap.merge f a b
+  and joinVarInfo a b =
+    match (a, b) with
+    | (NoInfo, other) -> other
+    | (other, NoInfo) -> other
+    | (BoolInfo aa, BoolInfo bb) ->
+      BoolInfo {
+        ifTrue = join aa.ifTrue bb.ifTrue;
+        ifFalse = join aa.ifFalse bb.ifFalse;
+      }
+    | (ScalarInfo aa, ScalarInfo bb) ->
+      ScalarInfo (SC.join aa bb)
+    | _ -> raise TypeMismatch
+
+  let rec meet a b =
+    let f _ a b =
+      match (a, b) with
+      | (Some aa, Some bb) -> Some (meetVarInfo aa bb)
+      | (Some _, None) -> a
+      | (None, Some _) -> b
+      | (None, None) -> raise Impossible
+    in VarMap.merge f a b
+  and meetVarInfo a b =
+    match (a, b) with
+    | (NoInfo, _) -> NoInfo
+    | (_, NoInfo) -> NoInfo
+    | (BoolInfo aa, BoolInfo bb) ->
+      BoolInfo {
+        ifTrue = meet aa.ifTrue bb.ifTrue;
+        ifFalse = meet aa.ifFalse bb.ifFalse;
+      }
+    | (ScalarInfo aa, ScalarInfo bb) ->
+      ScalarInfo (SC.meet aa bb)
+    | _ -> raise TypeMismatch
+
+  (* TODO: this is not correct since we have Other = _|_ *)
+  let leq a b = join a b = b
+end
+
+exception No_value
+let option_get opt =
+  match opt with
+  | Some x -> x
+  | None -> raise No_value
+
+let getVarInfo (var : Variable.t) (map : Lattice.t) =
+  Lattice.VarMap.find var map
+
+let rec add_constraints (known : Lattice.t) (flam : Flambda.t) : (Lattice.t * Lattice.varInfo) =
   match flam with
   | Flambda.Var v -> (known, getVarInfo v known)
   | Flambda.Let {Flambda.var; Flambda.defining_expr; Flambda.body; _} ->
-     let (known, deInfo) = add_constraints_named known defining_expr in
-     let known = VarMap.add var deInfo known in
-     add_constraints known body
+    let (known, deInfo) = add_constraints_named known defining_expr in
+    let known = Lattice.VarMap.add var deInfo known in
+    add_constraints known body
   (* Right now, we ignore mutable variables. no memory woohoo *)
   | Flambda.Let_mutable {Flambda.body; _} -> add_constraints known body
   | Flambda.Let_rec (defs, body) ->
-     let add_def known (name, def) =
-       let (known, defInfo) = add_constraints_named known def
-       in (VarMap.add name defInfo known) in
-     let known = List.fold_left add_def known defs in
+    let add_def known (name, def) =
+      let (known, defInfo) = add_constraints_named known def
+      in (Lattice.VarMap.add name defInfo known) in
+    let known = List.fold_left add_def known defs in
      add_constraints known body
   (* We don't known anything about functions right now, although maybe we ought to.
    * Note that in flambda, apply is var(var, var, ...) so we can't gleam any more info
    * by traversing. *)
-  | Flambda.Apply _ -> (known, Other)
+  | Flambda.Apply _ -> (known, Lattice.NoInfo)
   (* Object oriented stuff, ew *)
-  | Flambda.Send _ -> (known, Other)
+  | Flambda.Send _ -> (known, Lattice.NoInfo)
   (* Assignment of a mutable variable. We might want to return to this later *)
-  | Flambda.Assign _ -> (known, Other)
+  | Flambda.Assign _ -> (known, Lattice.NoInfo)
   | Flambda.If_then_else (_, trueBranch, falseBranch) ->
-     (* TODO: Do we need to add the iftrue/iffalse information in some fashion 
-      * while analyzing the branches? *)
-     let (known, trueInfo) = add_constraints known trueBranch in
-     let (known, falseInfo) = add_constraints known falseBranch in
-     (known, merge_var_info trueInfo falseInfo)
+    (* TODO: Do we need to add the iftrue/iffalse information in some fashion
+     * while analyzing the branches?
+     * Jacob: yes *)
+    let (sigmaTrue, trueInfo) = add_constraints known trueBranch in
+    let (sigmaFalse, falseInfo) = add_constraints known falseBranch in
+    let sigmaBoth = Lattice.join sigmaTrue sigmaFalse in
+    let sigmaNew = Lattice.join known sigmaBoth in
+    (sigmaNew, Lattice.joinVarInfo trueInfo falseInfo)
   | Flambda.Switch (_, {Flambda.consts; Flambda.blocks; Flambda.failaction}) ->
-     let add_case (known, possibleInfos) (_, case) =
-       let (known, caseInfo) = add_constraints known case
-       in (known, caseInfo :: possibleInfos)
-     in
-     let (known, possibleInfos) =
-       List.fold_left add_case (List.fold_left add_case (known, []) consts) blocks
-     in
-     let switchInfo = List.fold_left merge_var_info Other possibleInfos in
-     (match failaction with
+    let add_case (known, possibleInfos) (_, case) =
+      let (known2, caseInfo) = add_constraints known case
+      in (Lattice.join known known2, caseInfo :: possibleInfos)
+    in
+    let (known, possibleInfos) = List.fold_left add_case (known, []) consts in
+    let (known, possibleInfos) = List.fold_left add_case (known, possibleInfos) blocks in
+    let switchInfo = match possibleInfos with
+                      | x::xs -> List.fold_right Lattice.joinVarInfo xs x
+                      | [] -> Lattice.NoInfo in
+    begin match failaction with
      | None -> (known, switchInfo)
      | Some action -> let (known, actionInfo) = add_constraints known action in
-                      (known, merge_var_info switchInfo actionInfo))
+                      (known, Lattice.joinVarInfo switchInfo actionInfo)
+    end
   | Flambda.String_switch (_, cases, fallthrough) ->
-     let add_case (known, possibleInfos) (_, case) = 
-       let (known, caseInfo) = add_constraints known case
-       in (known, caseInfo :: possibleInfos) in
-     let (known, possibleInfos) = List.fold_left add_case (known, []) cases in
-     let switchInfo = List.fold_left merge_var_info Other possibleInfos in
-     (match fallthrough with
-      | None -> (known, switchInfo)
-      | Some action -> let (known, actionInfo) = add_constraints known action in
-                       (known, merge_var_info switchInfo actionInfo))
+    let add_case (known, possibleInfos) (_, case) =
+      let (known2, caseInfo) = add_constraints known case
+      in (Lattice.join known known2, caseInfo :: possibleInfos) in
+    let (known, possibleInfos) = List.fold_left add_case (known, []) cases in
+    let switchInfo = match possibleInfos with
+                      | x::xs -> List.fold_right Lattice.joinVarInfo xs x
+                      | [] -> Lattice.NoInfo in
+    begin match fallthrough with
+     | None -> (known, switchInfo)
+     | Some action -> let (known, actionInfo) = add_constraints known action in
+                      (known, Lattice.joinVarInfo switchInfo actionInfo)
+    end
+    (* let switchInfo = List.fold_left Lattice.join Other possibleInfos in
+    (match fallthrough with
+     | Lattice.NoInfo -> (known, switchInfo)
+     | Some action -> let (known, actionInfo) = add_constraints known action in
+                      (known, Lattice.join switchInfo actionInfo)) *)
   | Flambda.Try_with (exp, _, handler) ->
-     let (known, expInfo) = add_constraints known exp in
-     let (known, handlerInfo) = add_constraints known handler in
-     (known, merge_var_info expInfo handlerInfo)
+    let (known, expInfo) = add_constraints known exp in
+    let (known, handlerInfo) = add_constraints known handler in
+    (known, Lattice.joinVarInfo expInfo handlerInfo)
   | Flambda.While (cond, body) ->
-     let (known, _) = add_constraints known cond in
-     let (known, _) = add_constraints known body in
-     (* In general, we can't know anything about a while loop because who knows if the 
-      * condition will ever be true *)
-     (known, Other)
+    let (known, _) = add_constraints known cond in
+    let (known, _) = add_constraints known body in
+    (* In general, we can't know anything about a while loop because who knows if the
+     * condition will ever be true *)
+    (known, Lattice.NoInfo)
 
   | Flambda.For { Flambda.body; _; } -> add_constraints known body
-  | Flambda.Proved_unreachable -> (known, Other)
-  | _ -> (known, Other)
-and add_constraints_named (known : lattice) (named : Flambda.named) : (lattice * varInfo) =
+  | Flambda.Proved_unreachable -> (known, Lattice.NoInfo)
+  | _ -> (known, Lattice.NoInfo)
+and add_constraints_named (known : Lattice.t) (named : Flambda.named) : (Lattice.t * Lattice.varInfo) =
   match named with
   (* TODO implement properly *)
-  | Flambda.Symbol _ -> (known, Other)
-  | Flambda.Const _ -> (known, Other)
-  | Flambda.Allocated_const _ -> (known, Other)
-  | _ -> (known, Other)
+  | Flambda.Symbol _ -> (known, Lattice.NoInfo)
+  | Flambda.Const _ -> (known, Lattice.NoInfo)
+  | Flambda.Allocated_const _ -> (known, Lattice.NoInfo)
+  | _ -> (known, Lattice.NoInfo)
